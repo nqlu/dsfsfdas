cb1496ac71263d4565a7b8100dc62e631c791855
Quang Lu <nqlu@deakin.edu.au>
2018-09-10 00:52:12 +1000

    Add project files.

---
+++ FitnessWarrors/FitnessWarrors/View/LoginPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using FitnessWarriors.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+//h
+namespace FitnessWarriors.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+        void SignInProcedure(object sender, EventArgs e)
+        {
+            User user = new User(Entry_Username.Text, Entry_Password.Text);
+            if (user.CheckInformation())
+            {
+                DisplayAlert("Login", "Login Success", "Okay");
+            }
+            else
+            {
+                DisplayAlert("Login", "Login Failed", "Okay");
+            }
+        }
+
+    }
+}
